c695375264f65acadf4cf0a6406c75a5ac9353ff
Dipras <dipras75@gmail.com>
2024-03-29 20:20:40 +0700

    refactor: add index key each data map

--- app/screens/CourseScreen.tsx
+++ app/screens/CourseScreen.tsx
@@ -55,8 +55,8 @@ export const CourseScreen: FC<CourseScreenProps> = observer(function Course(_pro
                 <Image source={courseBanner} style={{ width: windowWidth, height: windowWidth / courseBannerRatio }} />
             </View>
             <View style={courseListStyle}>
-                {data?.pages.map((page, pageNum) => page.data.map((val: any) => (
-                    <TouchableOpacity onPress={() => _props.navigation.navigate("CourseDetail", {id: Number(val.id)})} style={item}>
+                {data?.pages.map((page, pageNum) => page.data.map((val: any, index: number) => (
+                    <TouchableOpacity onPress={() => _props.navigation.navigate("CourseDetail", {id: Number(val.id)})} style={item} key={index}>
                         <Image source={{ uri: val.images }} style={{ width: "100%", height: spacing.xxxl * 3 }} />
                         <View style={{ padding: spacing.sm }}>
                             <Text weight="bold" numberOfLines={2} style={{ height: spacing.xxl }}>{val.name}</Text>
