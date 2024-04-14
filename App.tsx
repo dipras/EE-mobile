71ac7b4fc9763140255b2489d17fef9c637309bd
Dipras <dipras75@gmail.com>
2024-04-14 15:47:34 +0700

    Add notification firebase package

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
 import App from "./app/app"
-import React from "react"
+import React, {useEffect, useRef} from "react"
 import * as SplashScreen from "expo-splash-screen"
+import * as Notifications from 'expo-notifications';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 SplashScreen.preventAutoHideAsync()
 
 function IgniteApp() {
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+
+  useEffect(() => {
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      // setNotification(notification);
+      console.log(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return <App hideSplashScreen={SplashScreen.hideAsync} />
 }
 
